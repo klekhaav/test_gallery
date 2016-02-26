5cde42dcc57f089513b14c0659878bb096e27bfd
Alexander Klekha <alexander.klekha@gmail.com>
2016-02-26 14:41:35 +0200

    Types and style of exhibit added.

--- gallery/admin.py
+++ gallery/admin.py
@@ -9,7 +9,8 @@ class ExhibitAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Exhibit',         {'fields':  ['title', 'author', 'original_img', 'image_thumb']}),
         ('Description',     {'fields':  ['creation_date', 'country']}),
-        ('Specification',   {'fields':  ['style', 'main_colour', 'pub_date']}),
+        ('Specification',   {'fields':  ['ex_type', 'main_colour']}),
+        ('Publication date', {'fields': ['pub_date']}),
     ]
     readonly_fields = ['image_thumb']
     list_display = ('title', 'author', 'pub_date', 'creation_date', 'image_thumb', 'was_published_recently')
@@ -27,5 +28,7 @@ class AuthorsAdmin(admin.ModelAdmin):
     search_fields = ('last_name', 'first_name', 'email')
     list_filter = ('with_us',)
 
+
 admin.site.register(Author, AuthorsAdmin)
 admin.site.register(Exhibit, ExhibitAdmin)
+
--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
 from sorl.thumbnail import ImageField, get_thumbnail
-from django.utils import timezone
 import datetime
 
 # Create your models here.
@@ -26,7 +25,65 @@ class Exhibit(models.Model):
     title = models.CharField(max_length=30)
     creation_date = models.DateField()
     country = models.CharField(max_length=30)
-    style = models.CharField(max_length=30)
+    author = models.ForeignKey(Author)
+    pub_date = models.DateField('date published')
+    original_img = ImageField(upload_to=dir_name)
+
+    # Exhibit types
+    PAINTING = 'Paint'
+    PHOTOGRAPHY = 'Photo'
+    DRAWING = 'Draw'
+    SCULPTURE = 'Sculpt'
+    COLLAGE = 'Collage'
+    PRINT = 'Print'
+
+    EX_TYPE_CHOICES = (
+        (PAINTING, (
+            ('Fine Art', 'Fine Art'),
+            ('Abstract', 'Abstract'),
+            ('Modern', 'Modern'),
+            ('Street Art', 'Street Art'),
+            ('Pop Art', 'Pop Art'),)
+        ),
+        (PHOTOGRAPHY, (
+            ('Fine Art', 'Fine Art'),
+            ('Portraiture', 'Portraiture'),
+            ('Abstract', 'Abstract'),
+            ('Documentary', 'Documentary'),
+            ('Conceptual','Conceptual'),)
+        ),
+        (DRAWING, (
+            ('Graffiti', 'Graffiti'),
+            ('Abstract', 'Abstract'),
+            ('Fine Art', 'Fine Art'),
+            ('Pop Art', 'Pop Art'),
+            ('Surrealism', 'Surrealism'),)
+        ),
+        (SCULPTURE, (
+            ('Pop Art', 'Pop Art'),
+            ('Abstract', 'Abstract'),
+            ('Wall', 'Wall'),
+            ('Figurative', 'Figurative'),
+            ('Modern', 'Modern'),)
+        ),
+        (COLLAGE, (
+            ('Dada', 'Dada'),
+            ('Pop Art', 'Pop Art'),
+            ('Abstract', 'Abstract'),
+            ('Surrealism', 'Surrealism'),
+            ('Street Art', 'Street Art'),)
+        ),
+        (PRINT, (
+            ('Fine Art', 'Fine Art'),
+            ('Abstract', 'Abstract'),
+            ('Art Deco', 'Art Deco'),
+            ('Pop Art', 'Pop Art'),
+            ('Folk Art', 'Folk Art'),)
+        ),
+    )
+
+    ex_type = models.CharField(max_length=15, choices=EX_TYPE_CHOICES)
+    ex_type.short_description = 'Type and Style'
 
     #COLORS
     BLACK = '#000000'
@@ -58,24 +115,20 @@ class Exhibit(models.Model):
                                    choices=MAIN_COLOR_CHOICES,
                                    default=WHITE)
 
-    author = models.ForeignKey(Author)
-
-    pub_date = models.DateField('date published')
-
-    original_img = ImageField(upload_to=dir_name)
-
     def image_thumb(self):
         im = get_thumbnail(self.original_img, '150x150', crop='center', quality=99)
         return u'<img src="%s" />' % im.url
     image_thumb.short_description = 'Image'
     image_thumb.allow_tags = True
 
-    def __unicode__(self):
-        return self.title
-
     def was_published_recently(self):
         return self.pub_date <= datetime.date.today()
-
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published?'
+
+    def __unicode__(self):
+        return self.title
+
+
+
